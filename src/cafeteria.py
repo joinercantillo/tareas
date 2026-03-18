5df93c98d423ff5a45432559602d3feb210b7243
joinercantillo <jcantill55@gmail.com>
2026-03-17 22:32:53 -0500

    cafeteria added

--- src/cafeteria.py
+++ src/cafeteria.py
@@ -1,16 +1,8 @@
 print("BIENVENIDO A CAFETERIA ****")
-print("NUESTRAS BEBIDAS SON: \n -1- cafe=4000 \n -1- te = 3500 \n -1- jugo = 5000 ")
-bebidas = {"cafe": 4000, "te": 3500, "jugo":5000}
-opcion = int(input("escoja una opcion(1-3): "))
-if opcion == 1:
-    print("cuantas unidades necesita?")
-    sum1 = int(input())
-    print("su cuenta final es: ", sum1 * bebidas["cafe"] )
-elif opcion == 2:
-    print("cuantas unidades necesita?")
-    sum2 = int(input())
-    print("su cuenta final es: ", sum2 * bebidas["te"] )
-elif opcion == 3:
-    print("cuantas unidades necesita?")
-    sum3 = int(input())
-    print("su cuenta final es: ", sum3 * bebidas["jugo"] )
+bebidas = [{"name": "cafe", "price": 4000}, {"name": "te", "price": 3500}, {"name": "jugo", "price": 5000}]
+for i,opcion in enumerate(bebidas, start=1):
+    print(f'{i}, {opcion ["name"]}: {opcion ["price"]}')
+opcion_select = int(input("escoja una opcion(1-3): "))
+quantity = int(input("ingrese la cantidad: "))
+
+print(f'your total is: {bebidas[opcion_select-1]["price"]*quantity}')
